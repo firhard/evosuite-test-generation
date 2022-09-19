4f46c798cca97a157f238592907864dc52afc73b
firhard <firhard@gmail.com>
2022-09-19 15:12:51 +0100

    Store into temporary folder

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROJECT_PATH=$1
-ORDER=$2
-TEST_NUMBER=$3
-REPORT_PATH=$4
+PROJECT_PATH_SOURCE=$1
+PROJECT_PATH=$2
+ORDER=$3
+TEST_NUMBER=$4
+REPORT_PATH=$5
 ORDER_MOD=$(expr $ORDER % 2)
+
+cp -Rp $PROJECT_PATH_SOURCE $PROJECT_PATH
 
 if [ $ORDER -le 1 ]; then
     bash $SCRIPT_DIR/run_developer_written_tests.sh $PROJECT_PATH $ORDER_MOD $TEST_NUMBER $REPORT_PATH
