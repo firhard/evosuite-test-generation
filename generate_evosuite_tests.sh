c67421de5040d5161a248bb4614d829d89d12a8c
firhard <firhard@gmail.com>
2022-09-19 14:13:20 +0100

    Update evosuite generation to exit

--- generate_evosuite_tests.sh
+++ generate_evosuite_tests.sh
@@ -7,7 +7,12 @@ FLAKY_FILTER=$2
 # generate tests
 if [[ $FLAKY_FILTER == 0 ]] 
 then
-    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes -Dctg_cores=1 -Dctg_memory=1000  -Dctg_bests_folder=../evosuite-tests -continuous EXECUTE -Dctg_time_per_class=2
+    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes -Dctg_cores=1 -Dctg_memory=1000  -Dctg_bests_folder=$PROJECT_PATH/evosuite-tests -continuous EXECUTE -Dctg_time_per_class=2
+    EVOSUITE_TESTS= $(find $PROJECT_PATH/evosuite-tests -type f 2> /dev/null | wc -l)
+    if [[ $EVOSUITE_TESTS == 0 ]]
+    then 
+        exit 1; 
+    fi
 else
     java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite \
     -target $PROJECT_PATH/target/classes \
@@ -25,7 +30,12 @@ else
     -Dreset_static_final_fields=false \ 
     -Dctg_cores=1  \
     -Dctg_memory=1000 \
-    -Dctg_bests_folder=../evosuite-flaky \
+    -Dctg_bests_folder=$PROJECT_PATH/evosuite-flaky \
     -continuous EXECUTE \
     -Dctg_time_per_class=2
+    EVOSUITE_TESTS= $(find $PROJECT_PATH/evosuite-flaky -type f 2> /dev/null | wc -l)
+    if [[ $EVOSUITE_TESTS == 0 ]]
+    then 
+        exit 1; 
+    fi
 fi
