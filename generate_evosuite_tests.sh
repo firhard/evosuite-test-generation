445a285fc12d9f04ef5c40a6fff5a88753e8ddf0
firhard <firhard@gmail.com>
2022-09-19 14:17:31 +0100

    combine compile with generation

--- generate_evosuite_tests.sh
+++ generate_evosuite_tests.sh
@@ -1,4 +1,5 @@
 MY_PATH=$(dirname "$0")
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir $(pwd)/evosuite-tests
 
 PROJECT_PATH=$1
@@ -8,6 +9,30 @@ FLAKY_FILTER=$2
 if [[ $FLAKY_FILTER == 0 ]] 
 then
     java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes -Dctg_cores=1 -Dctg_memory=1000  -Dctg_bests_folder=$PROJECT_PATH/evosuite-tests -continuous EXECUTE -Dctg_time_per_class=2
+
+    testDEPENDENCIES=$(find $SCRIPT_DIR/dependencies -type f -name \*.jar | tr '\n' ':')
+
+    #set classpath to run developer-written test
+    export CLASSPATH=$PROJECT_PATH/target/classes:$PROJECT_PATH/evosuite-tests/:$SCRIPT_DIR/test:$testDEPENDENCIES:$PROJECT_PATH/target/test-classes
+
+    TESTS=$(find $PROJECT_PATH/evosuite-tests/ -type f  -name \*.java)
+    echo "Compiling EvoSuite tests"
+    for x in $TESTS; do
+        JAVA_RESPONSE="$(javac $x 2>&1)";
+        # remove test class that fail to compile
+        if [[ $JAVA_RESPONSE == *"error"* ]]; then
+            rm $x
+            if [[ "$x" == *"_scaffolding.java"* ]]; then
+                xTest=${x//_scaffolding.java/.java}
+                rm $xTest
+            else
+                xTestScaffolding=${x//.java/_scaffolding.java}
+                rm $xTestScaffolding
+            fi
+        fi
+    done
+    echo "EvoSuite Tests Compiled"
+
     EVOSUITE_TESTS= $(find $PROJECT_PATH/evosuite-tests -type f 2> /dev/null | wc -l)
     if [[ $EVOSUITE_TESTS == 0 ]]
     then 
@@ -33,9 +58,34 @@ else
     -Dctg_bests_folder=$PROJECT_PATH/evosuite-flaky \
     -continuous EXECUTE \
     -Dctg_time_per_class=2
+
+    testDEPENDENCIES=$(find $SCRIPT_DIR/dependencies -type f -name \*.jar | tr '\n' ':')
+
+    #set classpath to run developer-written test
+    export CLASSPATH=$PROJECT_PATH/target/classes:$PROJECT_PATH/evosuite-flaky/:$SCRIPT_DIR/test:$testDEPENDENCIES:$PROJECT_PATH/target/test-classes
+
+    TESTS=$(find $PROJECT_PATH/evosuite-flaky/ -type f  -name \*.java)
+    echo "Compiling EvoSuite tests"
+    for x in $TESTS; do
+        JAVA_RESPONSE="$(javac $x 2>&1)";
+        # remove test class that fail to compile
+        if [[ $JAVA_RESPONSE == *"error"* ]]; then
+            rm $x
+            if [[ "$x" == *"_scaffolding.java"* ]]; then
+                xTest=${x//_scaffolding.java/.java}
+                rm $xTest
+            else
+                xTestScaffolding=${x//.java/_scaffolding.java}
+                rm $xTestScaffolding
+            fi
+        fi
+    done
+    echo "EvoSuite Tests Compiled"
+
     EVOSUITE_TESTS= $(find $PROJECT_PATH/evosuite-flaky -type f 2> /dev/null | wc -l)
     if [[ $EVOSUITE_TESTS == 0 ]]
     then 
         exit 1; 
     fi
 fi
+
