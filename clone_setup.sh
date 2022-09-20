67af44b8e930b2fc8e499cdf75518e1c91d4b058
firhard <firhard@gmail.com>
2022-09-20 11:27:50 +0100

    Update clonesetup

--- clone_setup.sh
+++ clone_setup.sh
@@ -13,7 +13,7 @@ RESULT_DIR="/results"
 # -- PARSE ARGS
 PROJECT_URL=$1
 PROJECT_HASH=$2
-PROJECT_PATH=$3
+REPOSITORY_DIR=$3
 PROJECT_NAME=$(echo $PROJECT_URL | sed -e 's/.*\///g')
 
 # -- DEBUG OUTPUT
@@ -29,7 +29,6 @@ CWD=$(pwd)
 # debug_echo " CWD = ${CWD}"
 
 # -- CLONE / COPY REPO
-REPOSITORY_DIR="$PROJECT_PATH/${PROJECT_NAME}"
 debug_echo "Clone Repository into ${REPOSITORY_DIR}"
 if [[ $PROJECT_URL == http* ]]
 then
@@ -37,7 +36,7 @@ then
     if [[ -n "$PROJECT_HASH" ]]; then
         cd "${REPOSITORY_DIR}" || exit 1
         git reset --hard "${PROJECT_HASH}" || exit 1
-        cd "${PROJECT_PATH}" || exit 1
+        cd "${CWD}" || exit 1
     fi
     REPO_HASH=$(git --git-dir="${REPOSITORY_DIR}/.git" rev-parse HEAD)
 else
