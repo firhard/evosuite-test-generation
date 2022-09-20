57d96c4bc3e035c51f9f489df03573bf794a4ed4
firhard <firhard@gmail.com>
2022-09-20 13:10:16 +0100

    Update project modules

--- project_modules.py
+++ project_modules.py
@@ -1,13 +1,18 @@
 import os
+import sys
 
-projectPath = os.getcwd()
-lines = open(projectPath + '/mvn-compile.log','r').read().splitlines()
-compiledProjects = [projects for projects in lines if "/target/classes" in projects and "source file" in projects and "Compiling" in projects]
-projectPath = []
-for project in compiledProjects:
-    x = project.split(' ')
-    projectPath.append(x[len(x)-1])
+def project_modules(argv):
+    projectPath = argv[1]
+    lines = open(projectPath + '/mvn-compile.log','r').read().splitlines()
+    compiledProjects = [projects for projects in lines if "/target/classes" in projects and "source file" in projects and "Compiling" in projects]
+    projectPath = []
+    for project in compiledProjects:
+        x = project.split(' ')
+        projectPath.append(x[len(x)-1])
 
-projectPath = [paths + '/../..' for paths in projectPath]
-for path in projectPath:
-    print(path)
+    projectPath = [paths + '/../..' for paths in projectPath]
+    for path in projectPath:
+        print(path)
+
+if __name__ == "__main__":
+    project_modules(sys.argv)
--- project_modules.sh
+++ project_modules.sh
@@ -8,35 +8,9 @@ if [ ! -f "$PROJECT_PATH/mvn-compile.log" ]; then
     mvn compile -l mvn-compile.log -Drat.skip=true
 fi
 
-MODULE_PATH=$(python3 $SCRIPT_DIR/project_modules.py)
+MODULE_PATH=$(python3 $SCRIPT_DIR/project_modules.py $PROJECT_PATH)
 for value in $MODULE_PATH
 do 
-    cd $value
-
     #generate tests
-    bash $SCRIPT_DIR/generate_evosuite_tests.sh $PROJECT_PATH $FLAKY_FILTER
-
-    #compile generated tests
-    bash $SCRIPT_DIR/compile_evosuite_tests.sh $PROJECT_PATH
-
-    #update classpath
-    # testDEPENDENCIES=$(find $SCRIPT_DIR/dependencies -type f -name \*.jar | tr '\n' ':')
-    # export CLASSPATH=$1/target/classes:$1/evosuite-tests/:$SCRIPT_DIR/test:$testDEPENDENCIES:$1/target/test-classes
-    # TEST_CLASS=$(find $1/evosuite-tests -type f  -name \*.class -not -name \*$\* -not -name \*_scaffolding\*  | tr '\n' ',')
-    # tclass=${TEST_CLASS//$1\/evosuite-tests\//}
-    # tclass=${tclass//_scaffolding/}
-    # tclass=${tclass//.class/}
-    # tclass=${tclass//\//.}
-
-    # mkdir $1/first-report
-    # echo "Run EvoSuite test once"
-    # java -Dclasses=${tclass} -Dorder=OD -DreportPath=$1/first-report EvoSuiteTestRunner 
-    
-    # XML_REPORT=$(find $1/first-report -type f  -name \*.xml)
-    # # comment out tests that are broken when being run once
-    # perl $SCRIPT_DIR/rm_broken_tests.pl $XML_REPORT $1/evosuite-tests
-    # rm -r $1/first-report
-
-    # # compile non-failing tests only
-    # bash $SCRIPT_DIR/compile_evosuite_tests.sh
+    bash $SCRIPT_DIR/generate_evosuite_tests.sh $value $FLAKY_FILTER
 done
