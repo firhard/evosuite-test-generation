b6641ffd3590130396db6b7d65b7d6537efd9d1f
firhard <firhard@gmail.com>
2022-09-20 15:24:53 +0100

    Update time and change variable name

--- generate_evosuite_tests.sh
+++ generate_evosuite_tests.sh
@@ -8,7 +8,7 @@ mkdir $PROJECT_PATH/evosuite-tests
 # generate tests
 if [[ $FLAKY_FILTER == 0 ]] 
 then
-    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes/ -Dctg_cores=1 -Dctg_memory=1000 -Dctg_bests_folder=evosuite-tests -Dctg_dir=$PROJECT_PATH -continuous EXECUTE -Dctg_time_per_class=1
+    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes/ -Dctg_cores=1 -Dctg_memory=1000 -Dctg_bests_folder=evosuite-tests -Dctg_dir=$PROJECT_PATH -continuous EXECUTE -Dctg_time_per_class=2
 
     testDEPENDENCIES=$(find ./dependencies -type f -name \*.jar | tr '\n' ':')
 
@@ -39,26 +39,7 @@ then
         exit 1; 
     fi
 else
-    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite \
-    -target $PROJECT_PATH/target/classes \
-    -Dtest_scaffolding=false \
-    -Dno_runtime_dependency=true \
-    -Djunit_check=false \
-    -Dsandbox=false \
-    -Dvirtual_fs=false \
-    -Dvirtual_net=false \
-    -Dreplace_calls=false \
-    -Dreplace_system_in=false \
-    -Dreplace_gui=false \
-    -Dreset_static_fields=false \
-    -Dreset_static_field_gets=false \
-    -Dreset_static_final_fields=false \ 
-    -Dctg_cores=1  \
-    -Dctg_memory=1000 \
-    -Dctg_bests_folder=evosuite-flaky \
-    -Dctg_dir=$PROJECT_PATH \
-    -continuous EXECUTE \
-    -Dctg_time_per_class=2
+    java -cp $MY_PATH/dependencies/evosuite-1.2.0.jar org.evosuite.EvoSuite -target $PROJECT_PATH/target/classes -Dtest_scaffolding=false -Dno_runtime_dependency=true -Djunit_check=false -Dsandbox=false -Dvirtual_fs=false -Dvirtual_net=false -Dreplace_calls=false -Dreplace_system_in=false -Dreplace_gui=false -Dreset_static_fields=false -Dreset_static_field_gets=false -Dreset_static_final_fields=false -Dctg_cores=1 -Dctg_memory=1000 -Dctg_bests_folder=evosuite-flaky -Dctg_dir=$PROJECT_PATH -continuous EXECUTE -Dctg_time_per_class=2
 
     testDEPENDENCIES=$(find ./dependencies -type f -name \*.jar | tr '\n' ':')
 
--- generate_evosuite_tests_module.sh
+++ generate_evosuite_tests_module.sh
@@ -9,9 +9,9 @@ if [ ! -f "$PROJECT_PATH/mvn-compile.log" ]; then
     mvn compile -l mvn-compile.log -Drat.skip=true
 fi
 
-MODULE_PATH=$(python3 ./project_modules.py $PROJECT_PATH)
-for value in $MODULE_PATH
+MODULES=$(python3 ./project_modules.py $PROJECT_PATH)
+for MODULE in $MODULES
 do 
     #generate tests
-    bash ./generate_evosuite_tests.sh $value $FLAKY_FILTER
+    bash ./generate_evosuite_tests.sh $MODULE $FLAKY_FILTER
 done
