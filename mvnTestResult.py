93db13364f815a1aa0c6d9136f2a82569911eaa3
firhard <firhard@gmail.com>
2022-10-05 13:33:59 +0100

    Remove print

--- mvnTestResult.py
+++ mvnTestResult.py
@@ -2,7 +2,6 @@ import sys
 import re
 
 result = sys.argv[1]
-
 result = re.sub('.*Tests run','Tests run', result)
 resultDict = dict(x.split(":") for x in result.split(", "))
 
@@ -16,7 +15,6 @@ if numTests == 0: exit(1)
 numNonPassTests = 0
 for key in resultDict:
     if key != "Tests run" and key != "Flakes" and key != "Failures":
-        print(key)
         numNonPassTests += resultDict[key]
 
 if numNonPassTests >= numTests: exit(1)
